3251de3651ca10ec5d4e3fe9c3d8bddf30004886
udaysarkarud <udaysarkarud@gmail.com>
2022-05-02 02:21:27 +0600

    footer section done

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ menu.onClick = () =>{
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
 }
+
+menu.onscroll = () =>{
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
